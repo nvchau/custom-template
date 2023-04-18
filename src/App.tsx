93135d9bf0a3472fd5b3636a49b747a7d8109bce
nvchau <nvchau.dev@gmail.com>
2023-04-18 20:44:22 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  const [a, setA] = useState(1)
+
+  useEffect(() => {
+    console.log(a);
+  }, [a])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
